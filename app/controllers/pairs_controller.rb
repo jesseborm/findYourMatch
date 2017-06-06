0b49c00bf8bcdeb3d8adaab586ab40a388c0febe
jesseborm <jesseborm@gmail.com>
2017-06-06 16:46:39 +0200

    Create Pairs and Clusters controller

---
+++ app/controllers/pairs_controller.rb
@@ -0,0 +1,7 @@
+class PairsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ spec/views/pairs/show.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "pairs/show.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
