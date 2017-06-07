98c2764e134d1a044f53256fae6fa5989740fe66
Hansterdam <hankortekaas@gmail.com>
2017-06-07 10:47:01 +0200

    Udate seeds and admin view

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ end
 
 User.create!(email: 'admin@match.com', password: '123qwf', admin: true)
 User.create!(email: 'student@match.com', password: '123qwf')
+
+cluster = Cluster.create!(day: '2017-06-07', assigned: true)
+
+Pair.create!(cluster: cluster, users: [User.find(1), User.find(2)])
+Pair.create!(cluster: cluster, users: [User.find(3), User.find(4)])
