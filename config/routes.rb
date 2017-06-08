d68b65d089f11a63ca91307df09918e311f1df0d
Hansterdam <hankortekaas@gmail.com>
2017-06-08 10:38:05 +0200

    Add see past matches view

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
 
   get 'pairs/show'
 
-
-
-  resources :clusters, only: [:index, :show, :update, :edit]
+  resources :clusters, only: %i[index show update edit]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
