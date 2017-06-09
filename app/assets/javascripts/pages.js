3193625f36a3201826556554c21b87a897b13c3f
Hansterdam <hankortekaas@gmail.com>
2017-06-09 09:55:57 +0200

    Generate table of pairs through ajax

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -4,7 +4,43 @@
 $(document).ready(function() {
   // debugger;
   var date = $('.today').data('date');
-  var today = date;
+  createTable(date);
+
+  $('.clickadyclick').on('click', showSelectPairsForm);
+  $('.clackadyclock').on('click', showViewPairsForm);
+  $('.glyphicon-chevron-right').on('click', displayNextAssignedPair);
+
+
+});
+
+function createTableRow(pair_id) {
+  $.ajax({
+    type: 'POST',
+    url: '/users/get_by_pair',
+    data: JSON.stringify({
+      id: pair_id
+    }),
+    contentType: 'application/json',
+    dataType: 'json'
+  })
+  .done(function(data) {
+    var first_user = $('<td></td>')
+      .html(data[0].email);
+
+    var second_user = $('<td></td>')
+      .html(data[1].email);
+
+    var tableRow = $('<tr>')
+      .attr('data-id', pair_id)
+      .append(first_user)
+      .append(second_user)
+      .append($('</tr>'));
+
+    $('table').append(tableRow);
+  })
+}
+
+function createTable(date) {
 
   $.ajax({
     type: 'POST',
@@ -15,33 +51,12 @@ $(document).ready(function() {
     contentType: 'application/json',
     dataType: 'json'
   })
-  .done(function(data) {
+  .done(function (data) {
     for(var i=0; i < Object.keys(data).length; i++) {
-      var id = data[i].id;
-      console.log(id);
-      $.ajax({
-        type: 'POST',
-        url: '/users/get_by_pair',
-        data: JSON.stringify({
-          id: id
-        }),
-        contentType: 'application/json',
-        dataType: 'json'
-      })
-      .done(function(data) {
-        console.log(data[0].email);
-        console.log(data[1].email);
-      })
+      createTableRow(data[i].id);
     }
   })
-
-  $('.clickadyclick').on('click', showSelectPairsForm);
-  $('.clackadyclock').on('click', showViewPairsForm);
-  $('.glyphicon-chevron-right').on('click', displayNextAssignedPair);
-
-
-});
-
+}
 
 function showViewPairsForm() {
   $('#view-pair-form').slideDown(350);
